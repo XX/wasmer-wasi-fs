db0052a6b65e1a44aa448154ab8812bb759c3b11
Alexander XX <freecoder.xx@gmail.com>
2021-01-30 18:57:46 +0300

    Simplified example

--- wasi_fs_example/src/lib.rs
+++ wasi_fs_example/src/lib.rs
@@ -1,10 +1,4 @@
-use std::{fs, io};
-
 #[no_mangle]
 pub fn start() {
-    fs::read_dir("/")
-        .unwrap()
-        .map(|res| res.map(|entry| entry.path()))
-        .collect::<Result<Vec<_>, io::Error>>()
-        .unwrap();
+    std::fs::read_dir("/").unwrap();
 }
